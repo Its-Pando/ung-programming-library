946a3a80efee4d134b95cbd0ad99eed1d04c4927
Carter <145875689+ItsPando@users.noreply.github.com>
2026-08-20 15:39:06 -0400

    test message

--- CSCI1140/Assignment_1.ps1
+++ CSCI1140/Assignment_1.ps1
@@ -11,9 +11,20 @@
 
 
 ### Question 3: File System and Directory Management
-Get-ChildItem C:\ -Force | Sort-Object Name | Select-Object Name, Mode, LastWriteTime
+# Get-ChildItem C:\ -Force | Sort-Object Name | Select-Object Name, Mode, LastWriteTime
 
 
+### Question 4: Process and System Resource Management
+# Get-Process | Select-Object ProcessName, ID, @{
+#     Name = "MemoryMB"
+#     Expression = {
+#         [Math]::Round($_.WS/1MB, 2)
+#     }
+# } -First 15 | Sort-Object MemoryMB -Descending
+
+
+### Question 5: File Hash Calculation in Powershell
+### Q5. 1
 
 
 
