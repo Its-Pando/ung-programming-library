7ce65f26cfe33d7f5940cacedb790e74e0b0b2ce
Its-Pando <cbray507@gmail.com>
2026-09-13 22:52:21 -0400

    finished assignment 2

--- CSCI1140/Assignment_2.ps1
+++ CSCI1140/Assignment_2.ps1
@@ -74,12 +74,41 @@
 
 
 ##### Question 5: File Integrity & Security
+# function ComputeFileHash {
+#     param (
+#         [string]$filePath
+#     )
+#     $filepath = Read-Host -Prompt "Enter the file path "
+#     $hash = Get-FileHash -Path $filePath -Algorithm SHA256
+#     $hash.Hash | Out-File -FilePath "C:\Users\cbray\Downloads\hashresult.txt"
+# }
 
+# function VerifyFileHash {
+#     param (
+#         [string]$filepath
+#     )
+#     $filepath = Read-Host -Prompt "Enter the file path to verify"
+#     $testhash = Get-FileHash -Path $filepath -Algorithm SHA256
+#     $originalhash = Get-Content -Path "C:\Users\cbray\Downloads\hashresult.txt"
+#     if ($testhash -eq $originalhash) {
+#         Write-Output "This file is authentic and has not been tampered with."
+#     } else {
+#         Write-Output "This file has been tampered with."
+#     }
+# }
 
+# ComputeFileHash
+# VerifyFileHash
+# | Format-Table -Property LocalAddress, LocalPort, RemoteAddress, RemotePort, State, OwningProcess, ID -AutoSize
 
-
-
-
+##### Question 6: Network Activity Monitoring
+Get-NetTCPConnection | 
+    Where-Object {
+        $_.RemoteAddress -notmatch "^192.168|10." -or $_.Port -notmatch "80|443|53"
+    } | 
+    Select-Object LocalAddress, LocalPort, RemoteAddress, RemotePort, State, @{Name = "ProcessName"; Expression = {(Get-Process -Id $_.OwningProcess -ErrorAction SilentlyContinue).ProcessName}
+    } | 
+    Format-Table -Property LocalAddress, LocalPort, RemoteAddress, RemotePort, State, OwningProcess, ID -AutoSize
 
 
 
